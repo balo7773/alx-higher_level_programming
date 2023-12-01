0701088bc884466d4820bce25e134883665eece1
balo7773 <balogunabdulmalik04@gmail.com>
2023-12-01 01:10:57 +0100

    learning

--- 7-edges.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-word = "Holberton"
-new_word = word
-word_first_3 = new_word[:3]
-word_last_2 = new_word[-2:]
-middle_word = new_word[1:-1]
-print(f"First 3 letters: {word_first_3}")
-print(f"Last 2 letters: {word_last_2}")
-print(f"Middle word: {middle_word}")
