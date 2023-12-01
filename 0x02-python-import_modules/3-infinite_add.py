eb4fbce3703e1549f37f060d54425e55fc67e5cd
balo7773 <balogunabdulmalik04@gmail.com>
2023-12-01 03:31:32 +0100

    learning python modules

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+
+    sum = 0
+    if len(sys.argv) == 1:
+        print(0)
+    elif len(sys.argv) > 1:
+        for i in range(1, len(sys.argv)):
+            sum = sum + int(sys.argv[i])
+        print("{:d}".format(sum))
+
