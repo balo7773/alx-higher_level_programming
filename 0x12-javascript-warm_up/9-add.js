20637bbc7482d9c9c826a99df4e4b72028348368
balogun abdulmalik <balogunabdulmalik04@gmail.com>
2024-06-20 03:06:27 +0100

    learning javascript

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,10 +1,12 @@
 #!/usr/bin/node
-const args = process.argv.slice(2);
-const num1 = Number(args[0]);
-const num2 = Number(args[1]);
-
 function add (a, b) {
-  return a + b;
+  console.log(a + b);
 }
 
-console.log(add(num1, num2));
+let firstArg = process.argv[2];
+let secondArg = process.argv[3];
+
+firstArg = parseInt(firstArg);
+secondArg = parseInt(secondArg);
+
+add(firstArg, secondArg);
